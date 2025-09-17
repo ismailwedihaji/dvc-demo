0e542d3c0d915499800748dba377bf8396218715
Ismail Mohammed <ismailwedihaji@gmail.com>
2025-09-17 11:06:04 +0200

    degree 2

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ X = np.arange(len(y)).reshape(-1, 1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
 
 # Polynomial regression
-degree = 3
+degree = 2
 poly = PolynomialFeatures(degree=degree)
 X_train_poly = poly.fit_transform(X_train)
 X_test_poly = poly.transform(X_test)
